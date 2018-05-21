69a1f2d2d167a2d40391c9a6e59d87f25cfbaa01
fenqiang.chen <fenqiang.chen@qlchat.com>
2018-05-21 20:08:01 +0800

    babel

--- server/configs/conf.js
+++ server/configs/conf.js
@@ -1,9 +1,12 @@
 import constant from './constant'
 
 export default {
-    /** 各种变量 */
+    // 各种变量
     ...constant,
-    /** 服务器启动的模式 */
+    /**
+     * 服务器启动的模式
+     * @type {constant.SERVER_MODES}
+     */
     MODE: process.env.NODE_ENV === 'production' ? constant.SERVER_MODES.PROD : constant.SERVER_MODES.DEV,
     /** 服务器监听的端口 */
     PORT: parseInt(process.env.PORT, 10) || 3000,
--- server/configs/constant.js
+++ server/configs/constant.js
@@ -1,7 +1,10 @@
 
 export default {
+    /** 服务端启动的模式 */
     SERVER_MODES: {
+        /** 线上环境模式 */
         PROD: 1,
+        /** 开发环境模式 */
         DEV: 2,
     }
 }
